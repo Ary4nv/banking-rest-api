b4848cd4aa4587fe012fb40b088b5e3ecfd8cb65
Arian Vares <arian@Arians-MacBook-Air.local>
2026-01-22 16:13:36 -0500

    transfer endpoints added

--- main.go
+++ main.go
@@ -30,6 +30,12 @@ type WithdrawInput struct{
 	Amount int `json:"amount"`
 }
 
+type Transfer struct {
+	From string `json:"from"`
+	To string `json:"to"`
+	Amount int `json:"amount"`
+}
+
 var accounts = map[string]Account{}
 var nextID = 1
 
@@ -80,7 +86,7 @@ func main() {
 		id := chi.URLParam(r,"id")
 		acc, found := accounts[id]
 		if !found {
-			http.Error(w,"user not found",http.StatusNotFound)
+			http.Error(w,"account not found",http.StatusNotFound)
 			return
 		}
 
@@ -137,7 +143,7 @@ func main() {
 			return
 		}
 		if dep.Amount <= 0 {
-			http.Error(w,"amount must be >0",http.StatusBadRequest)
+			http.Error(w,"amount must be > 0",http.StatusBadRequest)
 			return
 
 		}
@@ -164,12 +170,12 @@ func main() {
 		var wDraw WithdrawInput
 		err := json.NewDecoder(r.Body).Decode(&wDraw)
 		if err !=nil {
-			http.Error(w,"invalid Json",http.StatusBadRequest)
+			http.Error(w,"invalid JSON",http.StatusBadRequest)
 			return
 		}
 
 		if wDraw.Amount <=0 {
-			http.Error(w,"amount invalid : cant be 0 or lower",http.StatusBadRequest)
+			http.Error(w,"amount must be > 0",http.StatusBadRequest)
 			return
 		}
 
@@ -186,6 +192,58 @@ func main() {
 	
 		
 	
+
+	})
+
+	router.Post("/transfer", func(w http.ResponseWriter , r *http.Request){
+		w.Header().Set("Content-Type","application/json")
+		
+		var t Transfer
+		err := json.NewDecoder(r.Body).Decode(&t)
+		if err != nil{
+			http.Error(w, "invalid JSON", http.StatusBadRequest)
+			return
+		}		
+		if t.Amount <=0 {
+			http.Error(w, "invalid amount", http.StatusBadRequest)
+			return
+		}
+		if t.From == t.To {
+			http.Error(w, "cant transfer between same accounts", http.StatusBadRequest)
+			return
+		}
+		if t.From == "" || t.To == ""{
+			http.Error(w, "account id cant be empty", http.StatusBadRequest)
+			return
+
+		}
+
+		AccFrom, ok := accounts[t.From]
+		if !ok {
+			http.Error(w, "from account not found", http.StatusNotFound)
+			return
+		}
+		AccTo, ok := accounts[t.To]
+		if !ok {
+			http.Error(w, "destination account not found", http.StatusNotFound)
+			return
+		}
+		if AccFrom.Balance < t.Amount {
+			http.Error(w, "insufficient balance", http.StatusBadRequest)
+			return
+		}
+		AccFrom.Balance -= t.Amount
+		AccTo.Balance += t.Amount
+		
+		accounts[t.From] = AccFrom
+		accounts[t.To] = AccTo
+
+		w.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(w).Encode(map[string]Account{
+			"from" : AccFrom,
+			"to" : AccTo,
+
+		})
 
 	})
 
